d1e63a8f3c196881bb14836b8b0988d28076fba9
paresh2421 <pareshnair2411@gmail.com>
2025-10-01 16:45:09 +0530

    Adds image rendering logic

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,52 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url = 'https://api.unsplash.com/search/photos?client_id=aoOy7-Oj9ZbyKM_Jt_B3UDvnTo1m9DSEd1WP0qxGQ4&query=shake'
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async()=>{
+      const result = await axios.get(url)
+      return result.data;
+    }
+  })
+
+  console.log(response);
+  if(response.isLoading){
+    return(
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    )
+  }
+  
+  if(response.isError){
+    return(
+      <section className="image-container">
+        <h4>There was an error...</h4>
+      </section>
+    )
+  }
+  
+  const results = response.data.results
+  if(results.length < 1){
+    return(
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    )
+  }
+
   return (
-    <h1>Gallery</h1>
+    <section className="image-container">
+      {
+        results.map((item)=>{
+          const url = item?.urls?.regular;
+          return <img src={url} alt={item.alt_description} key={item.id}/>
+        })
+      }
+    </section>
   )
 }
 
